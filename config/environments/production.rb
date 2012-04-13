e7f5029ecc758e288854691f8fd1b340b219ee57
Todd Persen <todd.persen@gmail.com>
2012-04-13 01:10:43 -0400

    Get rid of default_charset in ActionMailer settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,6 @@ Rumulu::Application.configure do
   config.action_mailer.default_url_options = {:host => 'rumulu.com'}
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
